3c18f494495549a08e37400bc42990848b61dd05
Yana <yanaegorova69.3@gmail.com>
2021-10-26 01:20:32 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,281 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Stuffed Plush Toy',
+        price: 19.99,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Super soft marshmallow-like texture made of polyester fabric.',
+            },
+            {
+                text: 'Item measures 16 Inches of Lovable Cuddly Fun.',
+            },
+            {
+                text: 'Easily washable and dryable.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png', './img/img1-4.png', './img/img1-5.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Hand Puppets',
+        price: 24.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: '10" Velociraptor T-Rex Pteranodon Dinosaur Hand Puppet Bundle.',
+            },
+            {
+                text: 'Includes 10" Velociraptor, 10" Pteranodon and 10" T-Rex Bundle.',
+            },
+            {
+                text: 'Also Includes Bonus Animal Fun Facts.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Heel Wheels',
+        price: 35.32,
+        image: './img/img3.png',
+        description: [
+            {
+                text: '“Quick click” hook-and-loop strap system makes it a cinch to get rolling.',
+            },
+            {
+                text: 'Bright colored, light-up LED wheels add a flash of fun to your ride.',
+            },
+            {
+                text: 'Replaceable spark pads make sure you’re always ready to light it up.',
+            },
+            {
+                text: 'Heavy-duty, translucent, polycarbonate material is designed to withstand rugged use.',
+            },
+            {
+                text: 'Provides a snug, secure fit for U.S. shoe sizes youth 12 through adult 12.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png', './img/img3-5.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Giant Topping Tower',
+        price: 39.99,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Basic tower-building game includes 60 wooden stacking blocks.',
+            },
+            {
+                text: 'Jumbo size; can build towers measuring up to about 4 or 5 feet high.',
+            },
+            {
+                text: 'For indoor/outdoor use; fun for kids and adults.',
+            },
+            {
+                text: 'Includes a cotton bag for safely storing and easily transporting the blocks.',
+            },
+            {
+                text: 'Each block measures 5.9 by 1.2 by 1.4 inches.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png', './img/img4-5.jpg'],
+    },
+    {
+        id: 'product-5',
+        name: '2 Bubble Guns',
+        price: 43.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Super Value Pack. Bubble Guns Kit includes 2 Bubble Guns ( green and blue), 2 Bottles Bubble Solution and AA Batteries. （Notes：The bubble gun has special music effects, please take it carefully）.',
+            },
+            {
+                text: 'Easy to Use. Easy Attach. Super Durable. Fire a Barrage of Bubbles. Superior Quality.',
+            },
+            {
+                text: 'Super Fun: Perfect Bubble Toy and Family Fun and Outdoor Activity Toys.',
+            },
+            {
+                text: 'Super Value Summer Toy Pack for Party Favors. Perfect for Themed Party, Activity, Ceremony, Carnival, Birthday Parties, Party Favors, Camping Trips, Halloween, Weddings, School Classroom, Easter Basket Stuffers, Outdoor Water Toy Game Activities, Beach, Pack, Backyard Bubble Party and More!!!',
+            },
+            {
+                text: 'Child Safe: Non-Toxic. Solution Meet All Safety Tests (TRA, USP51, USP61 etc.). Very Safe on Kids’ Skin and Face.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Track',
+        price: 49.95,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Made in Germany; Constructed out of fade-resistant, high-quality ABS plastic, with no glue or screws.',
+            },
+            {
+                text: 'Doors and engine hood can be opened.',
+            },
+            {
+                text: 'Cab windows Made from transparent and shatter-proof plastic.',
+            },
+            {
+                text: 'Tow coupling with height-adjustable lifting gear.',
+            },
+            {
+                text: 'Innovative articulated steering; Crawler drive.',
+            },
+            {
+                text: 'Dimension 19.75 x 7.50 x 9.25 inch; compatible with all brooder figures (not included).',
+            },
+            {
+                text: 'Recommended age: 4 years old upward to play indoors and outdoors.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png','./img/img6-3.png','./img/img6-4.png','./img/img6-5.png','./img/img6-6.png','./img/img6-7.png','./img/img6-8.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Puzzle Board',
+        price: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: 'Extra large puzzle board surface with drawers for assembling, sorting, and storing puzzles.',
+            },
+            {
+                text: 'Durable, smooth wood construction gives you an even, dedicated surface for puzzle assembly.',
+            },
+            {
+                text: 'Suitable for puzzles up to 1,500 pieces.',
+            },
+            {
+                text: '4 pull-out drawers offer convenient storage space and easy access for sorted pieces.',
+            },
+            {
+                text: 'Easily store in-progress puzzles with a low-profile board design that can slide under furniture or be set in an out-of-the-way area between sessions.',
+            },
+            {
+                text: 'Dimensions: 35.5 x 26.4 x 0.13 inches (LxWxH).',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.png', './img/img7-4.png', './img/img7-5.png'],
+    },
+    {
+        id: 'product-8',
+        name: 'Talking Plush Horse',
+        price: 79.99,
+        image: './img/img8.png',
+        description: [
+            {
+                text: 'Press ears to hear the exclusive "I\'m a Little Pony" song, 6 talking phrases, and sound effects.',
+            },
+            {
+                text: 'Talks and sings with a synchronized moving mouth.',
+            },
+            {
+                text: 'Soft huggable plush.',
+            },
+            {
+                text: '3-position stirrups grow with your child.',
+            },
+            {
+                text: 'Sturdy steel frame.',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png', './img/img8-3.png', './img/img8-4.png', './img/img8-5.png', './img/img8-6.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Dragon Mask',
+        price: 84.41,
+        image: './img/img9.png',
+        description: [
+            {
+                text: 'Great for theater, cosplay, halloween or Renn fairs.',
+            },
+            {
+                text: 'Handmade in the USA.',
+            },
+            {
+                text: 'High quality: masks and costumes are designed to be comfortably worn for extended periods of time.',
+            },
+            {
+                text: 'Care instructions: hand Wash latex masks with diluted mild soap by lightly dabbing. Gently dab with a clean wet rag to rinse clean. Do not use oil based make-up, remover and creams as it will shorten the life of the latex. Non-oil based make-up should be fine.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Kiddieland Train',
+        price: 89.99,
+        image: './img/img10.png',
+        description: [
+            {
+                text: 'Motorized train moves forward, back, and stops as Mickey plays his favorite music with flashing lights!',
+            },
+            {
+                text: 'With fun activities on the side and a trailing cart which may be used to store toys.',
+            },
+            {
+                text: 'Hear realistic train engine revs, whistle, and horn for hours of driving fun.',
+            },
+            {
+                text: 'Ride the train on build-your-own tracks or ride it alone.',
+            },
+            {
+                text: 'Powered by readily available D cell batteries (included).',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png', './img/img10-4.png', './img/img10-5.png', './img/img10-6.png', './img/img10-7.png'],
+    },
+    {
+        id: 'product-11',
+        name: 'Princess Truck',
+        price: 119.98,
+        image: './img/img11.png',
+        description: [
+            {
+                text: 'Built in grip handle on roof for parent transportation. Includes floor board to protect little ones\' feet.',
+            },
+            {
+                text: 'Removable floor board for child powered imaginative play.',
+            },
+            {
+                text: 'Includes flatbed with a drop-down tailgate, rugged "off-road" wheels, gas cap that opens and closes, steering wheel with a working horn, distinctive truck styling with a realistic front grill, driver\'s door opens and closes.',
+            },
+            {
+                text: 'Assembly required.',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-2.png', './img/img11-2.png', './img/img11-2.jpeg'],
+    },
+    {
+        id: 'product-12',
+        name: 'Pedal Tractor',
+        price: 148.00,
+        image: './img/img12.png',
+        description: [
+            {
+                text: 'Fully functional Front Loader is capable of scooping up large piles of dirt, sand or snow; Front hood compartment lifts for authenticity.',
+            },
+            {
+                text: 'Tractor is pedal-powered, and no power system is required, the chain drive system is fully enclosed for safety; Oversized resin tires with long-lasting rubberized tread strips to ensure better traction.',
+            },
+            {
+                text: 'Detachable hauling trailer is fillable if your children want to use the tractor to fill the trailer with the Front Loader; Trailer can also be used to move toys, comic books and other items.',
+            },
+            {
+                text: 'High-impact blow molded resin body for superb style and durability; Two-position adjustable seat to be able to accommodate your children as they grow.',
+            },
+            {
+                text: 'Recommended for children between ages three and six years old; Maximum weight capacity of 50 pounds on seat of tractor.',
+            },
+        ],
+        images: ['./img/img12-1.png','./img/img12-2.png'],
+    },
+]
